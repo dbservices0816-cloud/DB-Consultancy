bf14d2d2940fddc4b569579794bb9faed9622556
DB <guptahimu90@gmail.com>
2026-08-31 14:59:09 +0530

    compliance tsx batter version

--- src/components/index.js
+++ src/components/index.js
@@ -26,22 +26,22 @@
 // Topic: Stack
 
 
-function isValid(s) {
-    const stack = [];
-    const map = {
-        '(': ')',
-        '{': '}',
-        '[': ']'
-    };
-    for (let char of s) {
-        if (map[char]) {
-            stack.push(char);
-        } else {
-            const last = stack.pop();
-            if (map[last] !== char) {
-                return false;
-            }
-        }
-    }
-    return stack.length === 0;
-}
+// function isValid(s) {
+//     const stack = [];
+//     const map = {
+//         '(': ')',
+//         '{': '}',
+//         '[': ']'
+//     };
+//     for (let char of s) {
+//         if (map[char]) {
+//             stack.push(char);
+//         } else {
+//             const last = stack.pop();
+//             if (map[last] !== char) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return stack.length === 0;
+// }
