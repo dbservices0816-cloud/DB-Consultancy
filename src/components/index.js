901a8ca874ed744ff400b28cefdc5ebe45908054
DB <guptahimu90@gmail.com>
2026-08-31 15:10:29 +0530

    inked List iteratively

--- src/components/index.js
+++ src/components/index.js
@@ -45,3 +45,33 @@
 //     }
 //     return stack.length === 0;
 // }
+
+
+
+
+
+
+
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+function reverseList(head) {
+    let prev = null;
+    let curr = head;
+
+    while (curr !== null) {
+        let next = curr.next; // next node save karo
+
+        curr.next = prev;     // pointer reverse karo
+
+        prev = curr;          // prev ko aage move karo
+        curr = next;          // curr ko aage move karo
+    }
+
+    return prev;
+}
