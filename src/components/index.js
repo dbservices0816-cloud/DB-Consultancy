ae9ba47b94c61cede2e19646f239cd76b8c8bba9
DB <guptahimu90@gmail.com>
2026-09-01 12:47:41 +0530

    inked List iteratively

--- src/components/index.js
+++ src/components/index.js
@@ -26,26 +26,27 @@
 // Topic: Stack
 
 
-// function isValid(s) {
-//     const stack = [];
-//     const map = {
-//         '(': ')',
-//         '{': '}',
-//         '[': ']'
-//     };
-//     for (let char of s) {
-//         if (map[char]) {
-//             stack.push(char);
-//         } else {
-//             const last = stack.pop();
-//             if (map[last] !== char) {
-//                 return false;
-//             }
-//         }
-//     }
-//     return stack.length === 0;
-// }
+function isValid(s) {
+    const stack = [];
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    for (let char of s) {
+        if (map[char]) {
+            stack.push(char);
+        } else {
+            const last = stack.pop();
+            if (map[last] !== char) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
 
+log(isValid("()[]{}")); // true
 
 
 
