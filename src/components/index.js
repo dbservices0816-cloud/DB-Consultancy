7bb79f2a2993a1ebac530c914f4b9dcd22527fd8
DB <guptahimu90@gmail.com>
2026-09-02 16:29:33 +0530

    two sum with new map

--- src/components/index.js
+++ src/components/index.js
@@ -4,21 +4,21 @@
 
 
 
-// function twoSum(nums, target) {
-//     const numMap = new Map();
-//     for (let i = 0; i < nums.length; i++) {
-//         const complement = target - nums[i];
-//         if (numMap.has(complement)) {
-//             return [numMap.get(complement), i];
-//         }
-//         numMap.set(nums[i], i);
-//     }
-//     return null;
+function twoSum(nums, target) {
+    const numMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+        numMap.set(nums[i], i);
+    }
+    return null;
 
-// }
-// twoSum([2, 7, 11, 15], 9);
+}
+twoSum([2, 7, 11, 15], 9);
 
-// console.log(twoSum);
+console.log(twoSum);
 
 
 
